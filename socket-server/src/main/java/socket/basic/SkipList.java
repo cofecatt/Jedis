bc97003c6aaa4c44c5c4b00d7d43ce8a641264e6
huanglijie <486438441@qq.com>
2022-05-30 22:19:20 +0800

    new

--- socket-server/src/main/java/socket/basic/SkipList.java
+++ socket-server/src/main/java/socket/basic/SkipList.java
@@ -13,21 +13,28 @@ public class SkipList<T> {
     /**
      * 跳表的头尾是head和tail结点
      */
-    private  final SkipListNode<T> head = new SkipListNode<>(-1, null);
-    private  final SkipListNode<T> tail = new SkipListNode<>(1, null);
+    private final SkipListNode<T> head = new SkipListNode<>(-1, null);
+    private final SkipListNode<T> tail = new SkipListNode<>(1, null);
+
     /**
-     * 跳表的自身参数
+     * 跳表的结点数（正无穷，负无穷，head、tail结点不算）
      */
-    // 跳表的结点数（正无穷，负无穷，head、tail结点不算）
-    private Integer nodes;
-    // 跳表的高度
+    private Integer nodeCount;
+
+    /**
+     * 跳表的高度
+     */
     private Integer height;
 
     /**
      * 随机性升层
      */
-    private Random random =new Random();
-    private static final double PROBABILITY=0.5;//向上提升一个的概率
+    private final Random random = new Random();
+
+    /**
+     * 向上提升一个的概率
+     */
+    private static final double PROBABILITY = 0.5;
 
     /**
      * 无参构造
@@ -36,35 +43,35 @@ public class SkipList<T> {
         // 创建一个新的空白层
         createNewList();
         // 初始化参数
-        height=0;
-        nodes=0;
+        height = 0;
+        nodeCount = 0;
     }
 
     /**
      * 获取当前跳表的节点数
-     * @return
+     * @return 节点数
      */
-    public Integer size(){
-        return nodes;
+    public Integer size() {
+        return nodeCount;
     }
 
     /**
      * 新增或者修改
      * @param key
      */
-    public void put(Integer key,T value){
+    public void put(Integer key, T value) {
         // 根据key查询跳表中是否存在对应的节点
         SkipListNode<T> p = findNode(key);
         // 存在节点就修改值
-        if (Objects.equals(key, p.getKey())){
+        if (Objects.equals(key, p.getKey())) {
             p.setValue(value);
         }
-        // 不存在就新增
-        SkipListNode<T> q = new SkipListNode<>(key, value);// 创建新增节点
+        // 不存在就新增,建新增节点
+        SkipListNode<T> q = new SkipListNode<>(key, value);
         // 底层链表新增节点
-        backLink(p,q);
+        backLink(p, q);
         // 对新增节点进行随机性升层操作
-        probabilityUpgrade(q,key);
+        probabilityUp(q, key);
     }
 
     /**
@@ -72,9 +79,9 @@ public class SkipList<T> {
      * @param key
      * @return
      */
-    public SkipListNode<T> search(Integer key){
+    public SkipListNode<T> search(Integer key) {
         SkipListNode<T> node = findNode(key);
-        if (Objects.equals(node.getKey(), key)){
+        if (Objects.equals(node.getKey(), key)) {
             return node;
         }
         return null;
@@ -84,16 +91,17 @@ public class SkipList<T> {
      * 根据key删除
      * @param key
      */
-    public void remove(Integer key){
+    @SuppressWarnings("unchecked")
+    public void remove(Integer key) {
         // 删除各层的节点
         SkipListNode<T> pointer = findNode(key);
-        if (!Objects.equals(pointer.getKey(), key)){
+        if (!Objects.equals(pointer.getKey(), key)) {
             return;
         }
-        while (true){
+        while (true) {
             // 前后的节点指针重新指向地址
-            horizontalLink(pointer.left,pointer.right);
-            if (pointer.up==null){
+            horizontalLink(pointer.left, pointer.right);
+            if (pointer.up == null) {
                 break;
             }
             pointer = pointer.up;
@@ -104,36 +112,39 @@ public class SkipList<T> {
     /**
      * 对新增节点进行随机性升层操作
      * @param q
-     * @param key
+     * @param key 键
      */
-    private void probabilityUpgrade(SkipListNode<T> q, Integer key) {
-        int currentLevel=0;// 当前所在层数为0，底层是用于存value的
+    @SuppressWarnings("unchecked")
+    private void probabilityUp(SkipListNode<T> q, Integer key) {
+        // 当前所在层数为0，底层是用于存value的
+        int currentLevel = 0;
         // 指针指向新增节点
-        SkipListNode<T> pointer =q;
+        SkipListNode<T> pointer = q;
         // 进行概率性的升层操作 随机数决定新增节点的高度
-        while (random.nextDouble() > PROBABILITY){
+        while (random.nextDouble() > PROBABILITY) {
             // 如果超出了高度，需要重新建一个顶层
-            if (currentLevel>=height){
+            if (currentLevel >= height) {
                 height++;
                 // 新建一个顶层
                 createNewList();
             }
-            //将p移动到上一层
-            SkipListNode<T> oldPointer =pointer;// 存储
-            while (pointer.up==null) {
-                pointer=pointer.left;
+            //将p移动到上一层存储
+            SkipListNode<T> oldPointer = pointer;
+            while (pointer.up == null) {
+                pointer = pointer.left;
             }
-            pointer=pointer.up; // 指针指向新增节点的上一层
+            // 指针指向新增节点的上一层
+            pointer = pointer.up;
             // 素引层是不需要value的只需保存key，用于查询即可
             SkipListNode<T> index = new SkipListNode<>(key, null);
             // 在这一层新增节点
-            backLink(pointer,index);
+            backLink(pointer, index);
             // 垂直双向连接索引节点和底层存值节点
             verticalLink(index, oldPointer);
             // 当前层数+1
             currentLevel++;
             // 指针上移到上一层
-            pointer=index;
+            pointer = index;
         }
     }
 
@@ -142,18 +153,18 @@ public class SkipList<T> {
      */
     private void createNewList() {
         // 跳表的每一层的头尾都是正负无穷的结点
-        SkipListNode<T> p1 = new SkipListNode<>(SkipListNode.negativeInfinity, null);
-        SkipListNode<T> p2 = new SkipListNode<>(SkipListNode.positiveInfinity, null);
+        SkipListNode<T> p1 = new SkipListNode<>(SkipListNode.NEGATIVEINTEGER, null);
+        SkipListNode<T> p2 = new SkipListNode<>(SkipListNode.POSITIVEINTEGER, null);
         // 水平横向连接
-        horizontalLink(p1,p2);
+        horizontalLink(p1, p2);
         // 由于初始化的时候跳表只有最底层，所以负无穷p1与head连接
-        if (head.down !=null && tail.down !=null){
-            verticalLink(p1,head.down);
-            verticalLink(p2,tail.down);
+        if (head.down != null && tail.down != null) {
+            verticalLink(p1, head.down);
+            verticalLink(p2, tail.down);
         }
-        verticalLink(this.head,p1);
-        // 垂直双向连接
-        verticalLink(this.tail,p2);
+        verticalLink(this.head, p1);
+        //垂直双向连接
+        verticalLink(this.tail, p2);
     }
 
     /**
@@ -162,8 +173,8 @@ public class SkipList<T> {
      * @param p2
      */
     private void verticalLink(SkipListNode<T> p1, SkipListNode<T> p2) {
-        p1.down=p2;
-        p2.up=p1;
+        p1.down = p2;
+        p2.up = p1;
     }
 
 
@@ -174,12 +185,12 @@ public class SkipList<T> {
      */
     private void backLink(SkipListNode<T> p, SkipListNode<T> q) {
         // 先保存p的右指针地址
-        SkipListNode right = p.right;
+        SkipListNode<T> right = p.right;
         // 横向连接p和新增节点q,q和right
-        horizontalLink(p,q);
-        horizontalLink(q,right);
+        horizontalLink(p, q);
+        horizontalLink(q, right);
         // 节点数加1
-        nodes++;
+        nodeCount++;
     }
 
     /**
@@ -189,46 +200,30 @@ public class SkipList<T> {
      */
     private SkipListNode<T> findNode(Integer key) {
         // 指针指向head,跳表的查询，从head或者tail开始
-        SkipListNode<T> pointer =head.down;
-        while (true){
+        SkipListNode<T> pointer = head.down;
+        while (true) {
             // 如果一层只有正负无穷，就没必要水平查找了
-            while (pointer.right.getKey()!=SkipListNode.positiveInfinity
-                    && pointer.right.getKey()<=key){
+            while (!pointer.right.getKey().equals(SkipListNode.POSITIVEINTEGER)
+                    && pointer.right.getKey() <= key) {
                 // 每层找最接近key的节点
-                pointer=pointer.right;
+                pointer = pointer.right;
             }
             // 水平找到最接近key的节点后，再垂直向下一层,然后继续直到最底层
-            if (pointer.down==null){
+            if (pointer.down == null) {
                 break;
             }
-            pointer=pointer.down;
+            pointer = pointer.down;
         }
         return pointer;
     }
 
     /**
-     * 水平横向连接 （p1在左，p2在右）
+     * 水平横向连接 p1 -> p2 and p1 <- p2
      * @param p1
      * @param p2
      */
     private void horizontalLink(SkipListNode<T> p1, SkipListNode<T> p2) {
-        p1.right=p2;
-        p2.left=p1;
-    }
-
-    public static void main(String[] args) {
-        SkipList<String> list = new SkipList<>();
-        list.put(2, "yan");
-        SkipListNode<String> search1 = list.search(2);
-        list.remove(2);
-        SkipListNode<String> search2 = list.search(2);
-        list.put(1, "wo");
-        list.put(3, "ai");
-        list.put(1, "cao");
-        list.put(4, "xiao");
-        list.put(6, "zhi");
-        list.put(5, "障");
-        System.out.println(search1.getValue());
-        System.out.println(search2);
+        p1.right = p2;
+        p2.left = p1;
     }
 }
